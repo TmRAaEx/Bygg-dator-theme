1d85247eca9816e36dce3cfc5135c55ee24336f2
tmraaex <tmr.aaex@gmail.com>
2025-09-17 02:05:44 +0200

    Did some front-page styling

--- front-page.php
+++ front-page.php
@@ -3,13 +3,13 @@
 
 
 
-<div id="hero" class="center-content">
-    <h1>Welcome to my shop!</h1>
-</div>
+<div id="hero" class="center-content"></div>
 
 <div class="center-content">
     <div class="content">
         <?php
+        $placeholder = wc_placeholder_img_src();
+
         $products = wc_get_products([
             'limit' => 5,
             'orderby' => 'date',
@@ -21,19 +21,18 @@
             <h2><?php echo __('Senaste Produkter!', 'pc-builds'); ?></h2>
 
             <?php if (!empty($products)): ?>
-                <ul class="pc_builds-list">
+                <ul class="grid-list">
                     <?php foreach ($products as $product): ?>
-                        <li class="pc_build-card">
+                        <li class="card product-card">
                             <a href="<?php echo esc_url(get_permalink($product->get_id())); ?>">
-                                <p class="pc_build-card-title"><?php echo esc_html($product->get_name()); ?></p>
-
-                                <div class="pc_build-card_image">
-                                    <?php echo $product->get_image('medium'); ?>
+                                <p class="card-title"><?php echo esc_html($product->get_name()); ?></p>
+                                <div class="card-image">
+                                    <?php echo $product->get_image('medium') ?: '<img src="' . esc_url($placeholder) . '" alt="Ingen bild">'; ?>
                                 </div>
-
-                                <p class="pc_build-card_price"><?php echo $product->get_price_html(); ?></p>
+                                <p class="card-meta"><?php echo $product->get_price_html(); ?></p>
                             </a>
                         </li>
+
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
@@ -51,22 +50,24 @@
             $pc_builds = new WP_Query($args);
 
             if ($pc_builds->have_posts()): ?>
-                <ul class="pc_builds-list">
+                <ul class="grid-list">
                     <?php while ($pc_builds->have_posts()):
                         $pc_builds->the_post(); ?>
 
-                        <li class="pc_build-card">
+                        <li class="card pc_build-card">
                             <a href="<?php the_permalink(); ?>">
-                                <p class="pc_build-card-title"><?php the_title() ?></>
-                                    <div class="pc_build-card_image">
-                                        <?php if (has_post_thumbnail()): ?>
-                                            <img src="<?php echo the_post_thumbnail_url() ?>"
-                                                alt="Bild på pc-build<?php the_title() ?>" />
-                                        <?php endif; ?>
-                                    </div>
-                                    <p class="pc_build-card_author"><?php the_author() ?></p>
+                                <p class="card-title"><?php the_title(); ?></p>
+                                <div class="card-image">
+                                    <?php if (has_post_thumbnail()): ?>
+                                        <?php the_post_thumbnail('medium'); ?>
+                                    <?php else: ?>
+                                        <img src="<?php echo esc_url($placeholder); ?>" alt="Ingen bild" />
+                                    <?php endif; ?>
+                                </div>
+                                <p class="card-meta"><?php the_author(); ?></p>
                             </a>
                         </li>
+
 
                     <?php endwhile; ?>
                 </ul>
