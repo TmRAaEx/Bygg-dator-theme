a6d45f38de38caf5b233969cc93b4bfadd1d18f3
tmraaex <tmr.aaex@gmail.com>
2025-09-18 17:55:34 +0200

    made tagmanager script readable

--- header.php
+++ header.php
@@ -3,15 +3,28 @@
 
 <head>
     <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-5S7N84N7');</script>
+    <script>
+        const gtm_id = 'GTM-5S7N84N7';  //wp-config is inside docker so just put this here
+        (function (window, document, scriptTag, dataLayerName, gtmId) {
+            window[dataLayerName] = window[dataLayerName] || [];
+            window[dataLayerName].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+
+            var firstScript = document.getElementsByTagName(scriptTag)[0];
+            var gtmScript = document.createElement(scriptTag);
+            var dlParam = dataLayerName !== 'dataLayer' ? '&l=' + dataLayerName : '';
+
+            gtmScript.async = true;
+            gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=' + gtmId + dlParam;
+
+            firstScript.parentNode.insertBefore(gtmScript, firstScript);
+
+        })(window, document, 'script', 'dataLayer', gtm_id);
+    </script>
     <!-- End Google Tag Manager -->
+
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
